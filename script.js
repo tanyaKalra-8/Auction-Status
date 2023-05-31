144bb77d218b7b6150ab7481a57b2d79c253cfc1
Taniya <tanya.kalra200@gmail.com>
2023-05-31 23:14:37 +0530

    project build completed

---
+++ script.js
@@ -0,0 +1,76 @@
+// https://gauravgitacc.github.io/postAppData/auctionData.json
+// let response = fetch(
+//   "https://gauravgitacc.github.io/postAppData/auctionData.json"
+// );
+
+//fetch returns a promise
+//that promise will indicate network call
+
+// response.then((serverResponse) => {
+//     //serverResponse is going to be instance of Response class
+//     //data will be encoded
+
+//     let data = serverResponse.json();
+
+//     //data is of promise again
+
+//     data.then((result) =>{
+//         console.log(result);
+//     })
+//     data.catch((error) =>{
+//         console.log("Failed to parse the data", error)
+//     });
+// })
+
+// response.catch((error) => {
+//     console.log("some error", error);
+// })
+
+//----------------------------------------------------
+
+let cardsContainer = document.getElementsByClassName("cards-container")[0];
+function addDataOnToUI(resultsList) {
+  //takes care of the Dom manipulation
+
+    for(let i=0;i<resultsList.length;i++){
+        let card = resultsList[i];
+
+        let innerCard = `
+            <div class="top">
+                <div class="status">
+                    <b class="status-chip ${card.status.toLowerCase()}">${card.status}</b>
+                    <span class="case-number">${card.caseNumber}</span>
+                </div>
+                <span class="date">${card.date}</span>
+            </div>
+            <div class="bottom">
+                <b class="from-location">${card.fromLocation}</b>
+                <span class="to-location">${card.toLocation}</span>
+            </div>
+    
+            <span class="price">${card.fare}</span>
+        `;
+    
+        let cardContainer = document.createElement("div")
+        cardContainer.className = "card";
+        cardContainer.innerHTML = innerCard;
+
+        cardsContainer.append(cardContainer)
+    }
+}
+
+//other way using asyn and await
+
+async function fetchData() {
+  try {
+    const result = await fetch(
+      "https://gauravgitacc.github.io/postAppData/auctionData.json"
+    );
+    const data = await result.json();
+    addDataOnToUI(data);
+  } catch (error) {
+    alert("Some error occured", error);
+  }
+}
+
+fetchData();
